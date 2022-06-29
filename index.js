7c6f9a17e9378cd6e0fd08ccec82791a3ecf540d
Justin <107945979+Thatmoonman@users.noreply.github.com>
2022-06-29 23:12:56 +0000

    Problems 1-3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("hey")
+//console.log("hey")
 
 function fizzbuzz(num) {
   //take in a number and return all of the numbers that are either divisible b three
@@ -31,7 +31,90 @@ function wizzbuzz(lowNum, highNum) {
 }
   
 
-console.log(fizzbuzz(30))
-console.log(fizzbuzz(10))
-console.log(fizzbuzz(12))
-(wizzbuzz(1, 30))
+//console.log(fizzbuzz(30))
+//console.log(fizzbuzz(10))
+//console.log(fizzbuzz(12))
+//wizzbuzz(1, 15)
+
+//problem set
+
+// Define a function `logBetween(lowNum, highNum)` that will print every number
+// from `lowNum` to `highNum`, inclusive. Inclusive means that the range includes
+// `lowNum` and `highNum`. Hint: this function only needs to print using `console.log`,
+// it does not need to return.
+//
+function logBetween(lowNum, highNum) {
+  for (i = lowNum; i <=highNum; i++) {
+    console.log(i)
+  }
+}
+// Examples:
+//
+//logBetween(-1, 2); // prints
+// -1
+// 0
+// 1
+// 2
+//
+//logBetween(14, 6); // prints nothing
+//
+//logBetween(4,6); // prints
+// 4
+// 5
+// 6
+
+//problem 2
+// Write a function `logBetweenStepper(min, max, step)` that takes in 3 numbers as
+// parameters. The function should print out numbers between `min` and `max` at `step`
+// intervals. See the following examples.
+//
+function logBetweenStepper(min, max, step) {
+  for (i=min; i<=max; i+=step) {
+    console.log(i);
+  }
+}
+// Examples:
+//
+//logBetweenStepper(5, 9, 1) // prints
+// 5
+// 6
+// 7
+// 8
+// 9
+//
+//logBetweenStepper(-10, 15, 5) // prints
+// -10
+// -5
+// 0
+// 5
+// 10
+// 15
+
+//Problem 3
+// Write a function `printFives(max)` that prints out the multiples of 5 that are
+// less than max.
+//
+// Try to solve this in two ways, using a conditional (if) and without using a
+// conditional
+//
+//function printFives(max) {
+  // for (i=0; i<max; i++) {
+  //   if (i % 5 === 0) {
+  //     console.log(i)
+  //   }
+  // }
+// }
+
+function printFives(max) {
+  for (i=0; i<max; i+=5) {
+    console.log(i)
+  }
+}
+// Example:
+//
+//printFives(20) // prints
+// 0
+// 5
+// 10
+// 15
+
