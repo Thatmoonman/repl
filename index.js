079618a8ff88f57e11ca4d92d7596133c224aeb3
Thatmoonman <>
2022-06-29 01:56:24 +0000

    challenge problem

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("hey")
+
+function fizzbuzz(num) {
+  //take in a number and return all of the numbers that are either divisible b three
+  //or five but not both
+  let arr = []
+  if (num % 3 === 0 && num % 5 === 0) {
+    return
+  } else {
+    if (num % 3 === 0) {
+      arr.push(num)
+    } else {
+      if (num % 5 === 0) {
+        arr.push(num)
+      }
+    }
+  }
+
+  return arr
+}
+// for (i=0, i<num,i++) {
+  
+// }
+console.log(fizzbuzz(30))
