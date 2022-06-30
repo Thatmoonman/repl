2c99a97f3b9391a020a68c056647c369ec15fd7c
Justin <107945979+Thatmoonman@users.noreply.github.com>
2022-06-30 15:00:31 +0000

    Pig Latin start

---
+++ Bonuswork.js
@@ -0,0 +1,33 @@
+//Bonus 1
+// Write a function `pigLatinize(word)` that translates a single word into its pig
+// latin equivalent. The rules for the translation are as follows:
+//
+// 1) For words that begin with a non-vowel, all letters before the initial
+// vowel are placed at the end of the word sequence. Then, "ay" is added to the end.
+//   Examples
+//   "pig" → "igpay"
+//   "banana" → "ananabay"
+//   "trash" → "ashtray
+//
+// 2) For words that begin with vowel sounds, just adds "yay" to the end
+//   Examples
+//   "eat" → "eatyay"
+//   "omelet" → "omeletyay"
+//   "are" → "areyay"
+//
+// Note: y should not be considered a vowel
+//
+function pigLatinize(word) {
+  let vowels = ["a","e","i","o","u"]
+  for (i=0;i<vowels.length;i++) {
+    if (word[0] === vowels[i]) {
+      return word + "yay";
+    }
+  } //cut first letter and then add back with +"way" at end of string
+}
+// Examples:
+//
+console.log(pigLatinize("teacher")); // => "eachertay"
+// pigLatinize("trash"); // => "ashtray"
+console.log(pigLatinize("eat")); // => "eatyay"
+// pigLatinize("orange"); // => "orangeyay"
